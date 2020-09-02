c41527c85ccbe5d5143b7a94d35f5d286eb337bb
root <root@coral.eskilltrading.com>
2020-09-02 12:38:02 +0200

    Reverted because change resulted in error 02/09/2020

--- eskill_custom/config/hr.py
+++ eskill_custom/config/hr.py
@@ -4,15 +4,19 @@ from frappe import _
 def get_data():
 	return [
 		{
-			{
-				"name": 'Employee Metrics',
-				"label": _('Employee Metrics'),
-				"type": 'link',
-				"link": '#Form/Employee Metrics/All Employees',
-				"category": "Employee",
-				"type": 'link',
-				"color": '#00FF00',
-				'standard': 1,
-			},
+			"label": _("Reports"),
+			"icon": "fa fa-list",
+			"items": [
+				{
+					"name": 'Employee Metrics',
+					"label": _('Employee Metrics'),
+					"type": 'link',
+					"link": '#Form/Employee Metrics/All Employees',
+                    "category": "Reports",
+                    "type": 'link',
+                    "color": '#00FF00',
+                    'standard': 1,
+                },
+			]
 		},
 	]
