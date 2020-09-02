ad2382ca3fe56a4bc9e0d783055aab2947b953f7
root <root@coral.eskilltrading.com>
2020-09-02 11:44:41 +0200

    02/09/2020

---
+++ eskill_custom/config/hr.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Reports"),
+			"icon": "fa fa-list",
+			"items": [
+				{
+					"name": 'Employee Metrics',
+					"label": _('Employee Metrics'),
+					"type": 'link',
+					"link": '#Form/Employee Metrics/All Employees',
+				},
+			]
+		},
+	]
