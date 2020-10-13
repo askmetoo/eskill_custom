d8b65fe4e244b549612e247171f5038ed7e63930
EskillTrading <eskilladmin@eskilltrading.com>
2020-10-13 16:08:08 +0200

    Adding issue types from RepairShopR

--- eskill_custom/hooks.py
+++ eskill_custom/hooks.py
@@ -129,4 +129,4 @@ app_license = "MIT"
 
 customDocs = ('Software Change', 'Software Change Log', 'Software Package', 'Software Instance', 'Component History', 'Warranty Swap Out')
 
-fixtures = [{'doctype': 'DocType', 'filters': [['name', 'in', customDocs]]}, {'doctype': 'Print Format', 'filters': [['name', 'like', 'Eskill%']]}]
+fixtures = [{'doctype': 'DocType', 'filters': [['name', 'in', customDocs]]}, {'doctype': 'Print Format', 'filters': [['name', 'like', 'Eskill%']]}, 'Issue Type']
