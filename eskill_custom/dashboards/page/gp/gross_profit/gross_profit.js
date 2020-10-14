44394ae87512574e98b684cf9744ca5a05f5337f
Archais <aipt321@gmail.com>
2020-10-14 14:44:33 +0200

    Removed indiviudal employee dashboards

--- eskill_custom/dashboards/page/gp/gross_profit/gross_profit.js
+++
@@ -1,8 +0,0 @@
-frappe.pages['gross-profit'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Gross Profit',
-		single_column: true
-	});
-	$(frappe.render_template('gross_profit')).appendTo(page.body);
-}
