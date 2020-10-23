537d1365d2e3f6b4c34ee799d42064e74f58426a
Archais <aipt321@gmail.com>
2020-10-23 16:07:57 +0200

    Permissions changes

--- eskill_custom/hooks.py
+++ eskill_custom/hooks.py
@@ -129,7 +129,7 @@ app_license = "MIT"
 
 modifiedDocs = ('Issue', 'Timesheet', 'Serial No', 'Material Request', 'Support Settings')
 
-fixtures = ['Custom Script', {'doctype': 'DocPerm', 'filters': [['parent', 'in', modifiedDocs]]}, 'Issue Type']
+fixtures = ['Custom Script', {'doctype': 'DocPerm', 'filters': [['parent', 'in', modifiedDocs]]}, {'doctype': 'Custom DocPerm', 'filters': [['parent', 'in', modifiedDocs]]}, 'Issue Type']
 
 website_context = {
         "favicon": "/assets/eskill_custom/images/EskillFavicon.png",
