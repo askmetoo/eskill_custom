cc465c57aa3b79c2ef15c5dc72788570e448c340
Archais <aipt321@gmail.com>
2021-04-09 15:43:21 +0200

    Created auction exchange rate table.

---
+++ eskill_custom/eskill_customisations/doctype/auction_exchange_rate/auction_exchange_rate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Eskill Trading and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Auction Exchange Rate', {
+	// refresh: function(frm) {
+
+	// }
+});
