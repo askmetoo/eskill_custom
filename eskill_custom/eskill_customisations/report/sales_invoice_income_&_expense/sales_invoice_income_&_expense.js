ecbd7dc8dc45ad8f38e488bd97950e1d609113b9
Archais <aipt321@gmail.com>
2021-05-03 12:00:05 +0200

    Created report for income & expense on sales.

---
+++ eskill_custom/eskill_customisations/report/sales_invoice_income_&_expense/sales_invoice_income_&_expense.js
@@ -0,0 +1,35 @@
+frappe.query_reports["Sales Invoice Income & Expense"] = {
+    "filters": [
+        {
+            "fieldname":"customer",
+            "label": __("Customer"),
+            "fieldtype": "Link",
+            "options": "Customer"
+        },
+        {
+            "fieldname":"customer_code",
+            "label": __("Customer Code"),
+            "fieldtype": "Data",
+        },
+        {
+            "fieldname":"currency",
+            "label": __("Currency"),
+            "fieldtype": "Select",
+            "options": " \nUSD\nRTGS"
+        },
+        {
+            "fieldname":"start_date",
+            "label": __("Start Date"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": frappe.datetime.month_start()
+        },
+        {
+            "fieldname":"end_date",
+            "label": __("End Date"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": frappe.datetime.month_end()
+        },
+    ]
+}
