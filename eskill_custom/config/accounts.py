ed7dba0c2a452b023b22a410473d16726e14c986
Archais <aipt321@gmail.com>
2021-05-03 12:00:33 +0200

    Added SI I&E report + Auction rate to Accounts.

---
+++ eskill_custom/config/accounts.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+from frappe import _
+import frappe
+
+
+def get_data():
+    config = [
+        {
+            "label": _("Multi Currency"),
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Auction Exchange Rate",
+                    "description": _("USD to RTGS exchange rate from RBZ.")
+                },
+            ]
+        },
+        {
+            "label": _("Profitability"),
+            "items": [
+            	{
+            		"type": "report",
+            		"name": "Sales Invoice Income & Expense",
+            		"doctype": "Sales Invoice",
+                    "is_query_report": True
+            	},
+            ]
+        }
+    ]
+
+    return config
