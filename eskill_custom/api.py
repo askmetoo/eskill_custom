e199e1cc48c0f92db4b99e5bb7cedf057d5e0c6a
Archais <aipt321@gmail.com>
2021-06-30 14:04:34 +0200

    Improved readability of SQL queries.

--- eskill_custom/api.py
+++ eskill_custom/api.py
@@ -8,7 +8,7 @@ def issue_total_hours(doctype, filters):
     "Return all time related to the given issue."
     try:
         hours_worked = frappe.db.sql(
-            "select round(sum(hours), 3) total, round(sum(billing_hours), 3) billable from `tabTimesheet Detail` where activity_doctype = 'Issue' and activity_document = '%s' and docstatus <> 2" % filters, as_dict=True)
+            "select round(sum(hours), 3) total, round(sum(billing_hours), 3) billable from `tabTimesheet Detail` where activity_doctype = 'Issue' and activity_document = '%s' and docstatus <> 2;" % filters, as_dict=True)
         return hours_worked
     except:
         return False
@@ -18,8 +18,15 @@ def issue_total_hours(doctype, filters):
 def check_maintain_stock(doctype, item):
     "Return maintain stock status."
     try:
-        maintain_stock = frappe.db.sql(
-            "select is_stock_item from tabItem where name = '%s' limit 1;" % item)
+        maintain_stock = frappe.db.sql(f"""\
+            select
+                is_stock_item
+            from
+                tabItem 
+            where
+                name = '{item}'
+            limit 1;"""
+        )
         return maintain_stock
     except:
         return False
@@ -29,12 +36,25 @@ def check_maintain_stock(doctype, item):
 def sales_invoice_tax(doctype, currency, customer):
     "Return tax template for Sales Invoice."
     try:
-        territory = frappe.db.sql(
-            f"select territory from tabCustomer where name = '{customer}';")
+        territory = frappe.db.sql(f"""\
+            select
+                territory
+            from
+                tabCustomer
+            where
+                name = '{customer}';"""
+        )
         if territory[0][0] != 'Zimbabwe':
             return None
-        template = frappe.db.sql(
-            f"select name from `tabSales Taxes and Charges Template` where currency = '{currency}' limit 1;")
+        template = frappe.db.sql(f"""\
+            select
+                name
+            from
+                `tabSales Taxes and Charges Template`
+            where
+                currency = '{currency}'
+            limit 1;"""
+        )
         return template
     except:
         return None
@@ -44,18 +64,48 @@ def sales_invoice_tax(doctype, currency, customer):
 def stock_lookup(doctype, user, item):
     "Returns stock locations and quantities."
     try:
-        stock = frappe.db.sql(
-            f"select (case when (W.warehouse_type in (select for_value from `tabUser Permission` where allow = 'Warehouse Type' and user = '{user}')"
-            f" or W.name in (select for_value from `tabUser Permission` where allow = 'Warehouse' and user = '{user}')) or"
-            f" ((select count(*) from `tabUser Permission` where (allow = 'Warehouse Type' or allow = 'Warehouse') and user = '{user}') = 0) then W.name else 'Other' end) location,"
-            f" sum(actual_qty) quantity"
-            f" from `tabStock Ledger Entry` as SLE"
-            f" join `tabItem` as I on SLE.item_code = I.name"
-            f" join `tabWarehouse` as W on SLE.warehouse =  W.name "
-            f" where I.is_stock_item and I.name = '{item}' and W.disabled = 0"
-            f" group by location"
-            f" having quantity > 0 or location = 'Other'"
-            f" order by (case when location = 'Other' then 1 else 0 end), quantity desc, location", as_dict = True
+        stock = frappe.db.sql(f"""\
+            select
+                (case when
+                    (W.warehouse_type in (select
+                        for_value
+                    from
+                        `tabUser Permission`
+                    where
+                        allow = 'Warehouse Type' and user = '{user}') or
+                    W.name in (select
+                        for_value
+                    from
+                        `tabUser Permission`
+                    where
+                        allow = 'Warehouse' and user = '{user}')) or
+                    ((select
+                        count(*)
+                    from
+                        `tabUser Permission`
+                    where
+                        (allow = 'Warehouse Type' or allow = 'Warehouse') and user = '{user}') = 0)
+                then
+                    W.name
+                else
+                    'Other'
+                end) location,
+                sum(actual_qty) quantity
+            from
+                `tabStock Ledger Entry` as SLE
+            join
+                `tabItem` as I on SLE.item_code = I.name
+            join
+                `tabWarehouse` as W on SLE.warehouse =  W.name 
+            where
+                I.is_stock_item and I.name = '{item}' and W.disabled = 0
+            group by
+                location
+            having
+                quantity > 0 or location = 'Other'
+            order by
+                (case when location = 'Other' then 1 else 0 end), quantity desc, location;""",
+            as_dict = True
         )
         return stock
     except:
@@ -66,12 +116,15 @@ def stock_lookup(doctype, user, item):
 def item_price_lookup(doctype, currency, price_list, item):
     "Returns the price of a given item."
     try:
-        price = frappe.db.sql(
-            f"select round(price_list_rate, 2)"
-            f" from `tabItem Price`"
-            f" where item_code = '{item}' and price_list = '{price_list}' and currency = '{currency}'"
-            f" order by creation desc"
-            f" limit 1"
+        price = frappe.db.sql(f"""\
+            select round(price_list_rate, 2)"
+            from
+                `tabItem Price`"
+            where
+                item_code = '{item}' and price_list = '{price_list}' and currency = '{currency}'"
+            order by
+                creation desc"
+            limit 1"""
         )
         return price if price else "Unavailable"
     except:
@@ -83,9 +136,16 @@ def auction_rate_lookup(posting_date: str) -> float:
     "Returns the auction rate at the date of SI posting."
 
     try:
-        auction_rate = frappe.db.sql(
-            f"select exchange_rate from `tabAuction Exchange Rate` where date < "
-            f"date_add('{posting_date}', interval 1 day) order by date desc limit 1"
+        auction_rate = frappe.db.sql(f"""\
+            select
+                exchange_rate
+            from
+                `tabAuction Exchange Rate`
+            where
+                date < date_add('{posting_date}', interval 1 day)
+            order by
+                date desc
+            limit 1"""
         )
         auction_rate = auction_rate[0][0]
     except:
@@ -112,14 +172,36 @@ def set_invoice_as_credited(credit):
 
     try:
         if credit:
-            invoice = frappe.db.sql(
-                "select parent Credit, CNI.delivery_note, DN.return_against, SII.Invoice from `tabSales Invoice Item` CNI "
-                "join (select name, return_against from `tabDelivery Note`) DN on DN.name = CNI.delivery_note "
-                "join (select parent Invoice, delivery_note from `tabSales Invoice Item` group by parent) SII on SII.delivery_note = DN.return_against "
-                "group by parent "
-                f"having Credit = '{credit}' "
-                "order by parent desc "
-                "limit 1;")
+            invoice = frappe.db.sql(f"""\
+                select
+                    parent Credit,
+                    CNI.delivery_note,
+                    DN.return_against,
+                    SII.Invoice
+                from
+                    `tabSales Invoice Item` CNI 
+                join
+                    (select
+                        name,
+                        return_against
+                    from
+                        `tabDelivery Note`) DN on DN.name = CNI.delivery_note 
+                join
+                    (select
+                        parent Invoice,
+                        delivery_note
+                    from
+                        `tabSales Invoice Item`
+                    group by
+                        parent) SII on SII.delivery_note = DN.return_against 
+                group by
+                    parent 
+                having
+                    Credit = '{credit}' 
+                order by
+                    parent desc 
+                limit 1;"""
+            )
             frappe.db.set_value("Sales Invoice", invoice[0][0], "return_against", invoice[0][3])
             frappe.db.set_value("Sales Invoice", invoice[0][3], "status", "Credit Note Issued")
         else:
@@ -144,12 +226,14 @@ def update_issue_billing(docfield: str, docname: str, docfield_status: str, issu
     Returns 1 to indicate normal operation."""
 
     frappe.db.sql(f"""\
-update
-    tabIssue
-set
-    {docfield} = '{docname}', {docfield}_status = '{docfield_status}'
-where
-    name = '{issue}'""")
+        update
+            tabIssue
+        set
+            {docfield} = '{docname}',
+            {docfield}_status = '{docfield_status}'
+        where
+            name = '{issue}';"""
+    )
     frappe.db.commit()
 
     return 1
@@ -162,22 +246,27 @@ def service_quote_ordered(issue: str) -> int:
     Returns 1 to indicate normal operation."""
 
     details = frappe.db.sql(f"""\
-select
-    'quotation', quotation, 'Ordered', name
-from
-    tabIssue
-where
-    name = '{issue}'""")[0]
+        select
+            'quotation',
+            quotation,
+            'Ordered',
+            name
+        from
+            tabIssue
+        where
+            name = '{issue}';"""
+    )[0]
 
     update_issue_billing(*details)
 
     frappe.db.sql(f"""\
-update
-    tabQuotation
-set
-    status = 'Ordered'
-where
-    name = '{details[1]}'""")
+        update
+            tabQuotation
+        set
+            status = 'Ordered'
+        where
+            name = '{details[1]}';"""
+    )
     frappe.db.commit()
 
     return 1
@@ -197,33 +286,37 @@ def invoice_sales_person(user: str, service_invoice: bool = False, issue: str = 
 
     if service_invoice:
         users = frappe.db.sql(f"""\
-select
-    TS.user user, sum(round(TS.total_billable_hours, 0)) time
-from
-    tabTimesheet TS 
-join
-    (select 
-        parent prnt 
-    from
-        `tabTimesheet Detail`
-    where 
-        activity_document = '{issue}') TSD on TSD.prnt = TS.name
-group by 
-    user
-having
-    time > 0""", as_dict=1)
+            select
+                TS.user user,
+                sum(round(TS.total_billable_hours, 0)) time
+            from
+                tabTimesheet TS 
+            join
+                (select 
+                    parent prnt 
+                from
+                    `tabTimesheet Detail`
+                where 
+                    activity_document = '{issue}') TSD on TSD.prnt = TS.name
+            group by 
+                user
+            having
+                time > 0;""",
+            as_dict=1
+        )
         time = frappe.db.sql(f"""\
-select
-    sum(round(TS.total_billable_hours, 0))
-from
-    tabTimesheet TS 
-join
-    (select 
-        parent prnt 
-    from
-        `tabTimesheet Detail`
-    where 
-        activity_document = '{issue}') TSD on TSD.prnt = TS.name""")[0][0]
+            select
+                sum(round(TS.total_billable_hours, 0))
+            from
+                tabTimesheet TS 
+            join
+                (select 
+                    parent prnt 
+                from
+                    `tabTimesheet Detail`
+                where 
+                    activity_document = '{issue}') TSD on TSD.prnt = TS.name;"""
+        )[0][0]
 
         if len(users) > 1:
             for i in range(len(users)):
@@ -233,14 +326,15 @@ join
         else:
             try:
                 user = frappe.db.sql(f"""\
-select
-    E.user_id
-from
-    tabEmployee E
-join
-    tabIssue I on I.current_technician = E.name
-Where
-    I.name = '{issue}'""")[0][0]
+                    select
+                        E.user_id
+                    from
+                        tabEmployee E
+                    join
+                        tabIssue I on I.current_technician = E.name
+                    Where
+                        I.name = '{issue}';"""
+                )[0][0]
             except:
                 pass
     if (service_invoice and len(users) == 0) or not service_invoice:
@@ -249,14 +343,17 @@ Where
 
     for user in users:
         sales_person = frappe.db.sql(f"""\
-select
-    SP.name 'sales_person', '{user['time']}' contribution
-from 
-    `tabSales Person` SP
-join 
-    tabEmployee E on E.name = SP.employee 
-where 
-    E.user_id = '{user['user']}'""", as_dict=1)[0]
+            select
+                SP.name 'sales_person',
+                '{user['time']}' contribution
+            from 
+                `tabSales Person` SP
+            join 
+                tabEmployee E on E.name = SP.employee 
+            where 
+                E.user_id = '{user['user']}';""",
+            as_dict=1
+        )[0]
         sales_team.append(sales_person)
 
     return sales_team
@@ -269,15 +366,16 @@ def non_billable_item(item_code: str, sla_job: int) -> 'dict[str, str | int]':
     results = {}
 
     valuation = frappe.db.sql(f"""\
-select
-    valuation_rate
-from
-    `tabStock Ledger Entry` SLE
-where
-    item_code = '{item_code}'
-order by
-    posting_date desc, posting_time desc
-limit 1""")
+        select
+            valuation_rate
+        from
+            `tabStock Ledger Entry` SLE
+        where
+            item_code = '{item_code}'
+        order by
+            posting_date desc, posting_time desc
+        limit 1;"""
+    )
 
     if len(valuation):
         results['valuation'] = valuation[0][0]
@@ -285,22 +383,24 @@ limit 1""")
         results['valuation'] = 0
 
     item_group = frappe.db.sql(f"""\
-select
-    item_group
-from
-    tabItem
-where
-    name = '{item_code}'""")[0][0]
+        select
+            item_group
+        from
+            tabItem
+        where
+            name = '{item_code}';"""
+    )[0][0]
 
     item_group = item_group.split(' ')[0]
 
     expense_account = frappe.db.sql(f"""\
-select
-    A.name
-from
-    tabAccount A
-where
-    account_name like '{'Cost of Sales - SLA ' if int(sla_job) else 'Warranty%'}{item_group}' and root_type = 'Expense'""")
+        select
+            A.name
+        from
+            tabAccount A
+        where
+            account_name like '{'Cost of Sales - SLA ' if int(sla_job) else 'Warranty%'}{item_group}' and root_type = 'Expense';"""
+    )
 
 
     if len(expense_account):
