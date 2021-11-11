5bdde708566ff6d86687843e85fe4c0a5ebea3ed
Archais <aipt321@gmail.com>
2021-11-11 11:05:40 +0200

    Added method to update the linked Service Order.

--- eskill_custom/eskill_customisations/doctype/warranty_swap_out/warranty_swap_out.py
+++ eskill_custom/eskill_customisations/doctype/warranty_swap_out/warranty_swap_out.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+
+import frappe
 from frappe.model.document import Document
 
+
 class WarrantySwapOut(Document):
-	pass
+    @frappe.whitelist()
+    def update_service_order(self):
+        service_device = frappe.get_doc("Service Device", self.service_device)
+        if self.docstatus != 2:
+            service_device.db_set("swap_out", self.name)
+        else:
+            service_device.db_set("swap_out", None)
