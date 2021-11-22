bcca322f78981a9b4940cb10d2250d5391116fd3
Archais <aipt321@gmail.com>
2021-11-22 12:21:30 +0200

    Initialised General Ledger (Multi-Currency) report.

---
+++ eskill_custom/eskill_customisations/report/general_ledger_(multi_currency)/general_ledger_(multi_currency).js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Eskill Trading and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["General Ledger (Multi-Currency)"] = {
+	"filters": [
+
+	]
+};
