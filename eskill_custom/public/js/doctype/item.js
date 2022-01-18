2d84118ffd08b4c2e43e7e7841e39e26dd1258cd
Archais <aipt321@gmail.com>
2022-01-18 12:02:20 +0000

    Added filters to filter the income and expense accounts in Item Defaults table.

--- eskill_custom/public/js/doctype/item.js
+++ eskill_custom/public/js/doctype/item.js
@@ -1,20 +1,54 @@
 frappe.ui.form.on('Item', {
     refresh(frm) {
-        frm.add_custom_button(__("KB Articles"), function() {
-            frm.save();
-            frappe.route_options = {
-                "type": "Product",
-                "product": cur_frm.doc.item_code
-            };
-            frappe.set_route("List", "KBA", "List");
-        });
-        frm.add_custom_button(__("New KBA"), function() {
-            frm.save();
-            frappe.route_options = {
-                "type": "Product",
-                "product": cur_frm.doc.item_code
-            };
-            frappe.set_route("Form", "KBA", "New KBA");
-        });
+        kba_buttons(frm);
+    },
+
+    onload(frm) {
+        account_filter(frm);
     }
 });
+
+
+function account_filter(frm) {
+    frm.fields_dict.item_defaults.grid.get_field("expense_account").get_query = function(doc, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        return {
+            filters: {
+                account_type: "Cost of Goods Sold",
+                company: row.company,
+                is_group: 0
+            }
+        }
+    };
+
+    frm.fields_dict.item_defaults.grid.get_field("income_account").get_query = function(doc, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        return {
+            filters: {
+                account_type: "Income Account",
+                company: row.company,
+                is_group: 0
+            }
+        }
+    };
+}
+
+
+function kba_buttons(frm) {
+    frm.add_custom_button(__("KB Articles"), function () {
+        frm.save();
+        frappe.route_options = {
+            "type": "Product",
+            "product": cur_frm.doc.item_code
+        };
+        frappe.set_route("List", "KBA", "List");
+    });
+    frm.add_custom_button(__("New KBA"), function () {
+        frm.save();
+        frappe.route_options = {
+            "type": "Product",
+            "product": cur_frm.doc.item_code
+        };
+        frappe.set_route("Form", "KBA", "New KBA");
+    });
+}
