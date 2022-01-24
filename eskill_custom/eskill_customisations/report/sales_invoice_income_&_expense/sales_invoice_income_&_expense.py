9f165f5fb971a76cb1339f237ac4f4d310c5d1fc
Archais <aipt321@gmail.com>
2022-01-24 12:55:18 +0000

    Changed indentation to spaces instead of tabs; Changed to customer and customer_name instead of customer_name and customer_code (old custom field).

--- eskill_custom/eskill_customisations/report/sales_invoice_income_&_expense/sales_invoice_income_&_expense.py
+++ eskill_custom/eskill_customisations/report/sales_invoice_income_&_expense/sales_invoice_income_&_expense.py
@@ -7,150 +7,150 @@ from frappe import _
 
 
 def execute(filters=None):
-	if filters:
-		conditions = get_conditions(filters)
-	else:
-		conditions= ''
+    if filters:
+        conditions = get_conditions(filters)
+    else:
+        conditions= ''
 
-	columns = [{
-			"fieldname": "invoice",
-			"label": _("Invoice"),
-			"fieldtype": "Link",
-			"options": "Sales Invoice",
-			"width": 100
-		},
-		{
-			"fieldname": "customer",
-			"label": _("Customer"),
-			"fieldtype": "Link",
-			"options": "Customer",
-			"width": 400
-		},
-		{
-			"fieldname": "customer_code",
-			"label": _("Customer Code"),
-			"fieldtype": "Text",
-			"width": 100
-		},
-		{
-			"fieldname": "posting_date",
-			"label": _("Posting Date"),
-			"fieldtype": "Date",
-			"width": 120
-		},
-		{
-			"fieldname": "currency",
-			"label": _("Invoice Currency"),
-			"fieldtype": "Text",
-			"width": 50
-		},
-		{
-			"fieldname": "cos_usd",
-			"label": _("Cost of Sales (USD)"),
-			"fieldtype": "Currency",
-			"width": 120
-		},
-		{
-			"fieldname": "net_total_usd",
-			"label": _("Income Excl. VAT (USD)"),
-			"fieldtype": "Currency",
-			"width": 120
-		},
-		{
-			"fieldname": "total_usd",
-			"label": _("Income Incl. VAT (USD)"),
-			"fieldtype": "Currency",
-			"width": 120
-		},
-		{
-			"fieldname": "rate",
-			"label": _("Rate (Auction/Exchange)"),
-			"fieldtype": "Text",
-			"width": 60
-		},
-		{
-			"fieldname": "cos_zwd",
-			"label": _("Cost of Sales (ZWD)"),
-			"fieldtype": "Currency",
-			"width": 120
-		},
-		{
-			"fieldname": "net_total_zwd",
-			"label": _("Income Excl. VAT (ZWD)"),
-			"fieldtype": "Currency",
-			"options": "",
-			"width": 120
-		},
-		{
-			"fieldname": "total_zwd",
-			"label": _("Income Incl. VAT (ZWD)"),
-			"fieldtype": "Currency",
-			"width": 120
-		}
-	]
-	data = frappe.db.sql(f"""select
-	`tabSales Invoice`.name as invoice,
-	`tabSales Invoice`.customer_name as customer,
-	tabCustomer.customer_code as customer_code,
-	`tabSales Invoice`.posting_date as posting_date,
-	`tabSales Invoice`.currency as currency,
-	(case when `tabSales Invoice`.is_return then GL.credit * -1 else GL.debit end) as cos_usd,
-	`tabSales Invoice`.base_net_total as net_total_usd,
-	`tabSales Invoice`.base_total as total_usd,
-	convert(rate.rate, char) as rate,
-	(case when `tabSales Invoice`.is_return then (GL.credit * rate.rate) * -1 else (GL.debit * rate.rate) end) as cos_zwd,
-	(`tabSales Invoice`.base_net_total * rate.rate) as net_total_zwd,
-	(`tabSales Invoice`.base_total * rate.rate) as total_zwd
+    columns = [{
+            "fieldname": "invoice",
+            "label": _("Invoice"),
+            "fieldtype": "Link",
+            "options": "Sales Invoice",
+            "width": 100
+        },
+        {
+            "fieldname": "customer",
+            "label": _("Customer"),
+            "fieldtype": "Link",
+            "options": "Customer",
+            "width": 400
+        },
+        {
+            "fieldname": "customer_name",
+            "label": _("Customer Code"),
+            "fieldtype": "Text",
+            "width": 100
+        },
+        {
+            "fieldname": "posting_date",
+            "label": _("Posting Date"),
+            "fieldtype": "Date",
+            "width": 120
+        },
+        {
+            "fieldname": "currency",
+            "label": _("Invoice Currency"),
+            "fieldtype": "Text",
+            "width": 50
+        },
+        {
+            "fieldname": "cos_usd",
+            "label": _("Cost of Sales (USD)"),
+            "fieldtype": "Currency",
+            "width": 120
+        },
+        {
+            "fieldname": "net_total_usd",
+            "label": _("Income Excl. VAT (USD)"),
+            "fieldtype": "Currency",
+            "width": 120
+        },
+        {
+            "fieldname": "total_usd",
+            "label": _("Income Incl. VAT (USD)"),
+            "fieldtype": "Currency",
+            "width": 120
+        },
+        {
+            "fieldname": "rate",
+            "label": _("Rate (Auction/Exchange)"),
+            "fieldtype": "Text",
+            "width": 60
+        },
+        {
+            "fieldname": "cos_zwd",
+            "label": _("Cost of Sales (ZWD)"),
+            "fieldtype": "Currency",
+            "width": 120
+        },
+        {
+            "fieldname": "net_total_zwd",
+            "label": _("Income Excl. VAT (ZWD)"),
+            "fieldtype": "Currency",
+            "options": "",
+            "width": 120
+        },
+        {
+            "fieldname": "total_zwd",
+            "label": _("Income Incl. VAT (ZWD)"),
+            "fieldtype": "Currency",
+            "width": 120
+        }
+    ]
+    data = frappe.db.sql(f"""select
+    `tabSales Invoice`.name as invoice,
+    `tabSales Invoice`.customer as customer,
+    tabCustomer.customer_name as customer_name,
+    `tabSales Invoice`.posting_date as posting_date,
+    `tabSales Invoice`.currency as currency,
+    (case when `tabSales Invoice`.is_return then GL.credit * -1 else GL.debit end) as cos_usd,
+    `tabSales Invoice`.base_net_total as net_total_usd,
+    `tabSales Invoice`.base_total as total_usd,
+    convert(rate.rate, char) as rate,
+    (case when `tabSales Invoice`.is_return then (GL.credit * rate.rate) * -1 else (GL.debit * rate.rate) end) as cos_zwd,
+    (`tabSales Invoice`.base_net_total * rate.rate) as net_total_zwd,
+    (`tabSales Invoice`.base_total * rate.rate) as total_zwd
 from
-	`tabSales Invoice`
+    `tabSales Invoice`
 left join
-	tabCustomer on tabCustomer.name = `tabSales Invoice`.customer 
+    tabCustomer on tabCustomer.name = `tabSales Invoice`.customer 
 join
-	(select
-		parent invoice,
-		delivery_note
-	from
-		`tabSales Invoice Item`
-	where
-		item_code is not null
-	group by
-		invoice) as link on `tabSales Invoice`.name = link.invoice
+    (select
+        parent invoice,
+        delivery_note
+    from
+        `tabSales Invoice Item`
+    where
+        item_code is not null
+    group by
+        invoice) as link on `tabSales Invoice`.name = link.invoice
 left join
-	(select
-		voucher_no,
-		sum(debit) debit,
-		sum(credit) credit
-	from
-		`tabGL Entry`
-	where
-		account like '%Cost of Sales%' and voucher_type = 'Delivery Note'
-	group by
-		voucher_no) as GL on GL.voucher_no = link.delivery_note
+    (select
+        voucher_no,
+        sum(debit) debit,
+        sum(credit) credit
+    from
+        `tabGL Entry`
+    where
+        account like '%Cost of Sales%' and voucher_type = 'Delivery Note'
+    group by
+        voucher_no) as GL on GL.voucher_no = link.delivery_note
 join
-	(select
-		name invoice,
-		(case when currency = 'USD' then auction_bid_rate else 1 / conversion_rate end) rate
-	from
-		`tabSales Invoice`) as rate on rate.invoice = `tabSales Invoice`.name 
+    (select
+        name invoice,
+        (case when currency = 'USD' then auction_bid_rate else 1 / conversion_rate end) rate
+    from
+        `tabSales Invoice`) as rate on rate.invoice = `tabSales Invoice`.name 
 where
-	`tabSales Invoice`.docstatus = 1 {conditions}
+    `tabSales Invoice`.docstatus = 1 {conditions}
 order by
-	`tabSales Invoice`.name desc
+    `tabSales Invoice`.name desc
 """, as_dict=1)
 
-	return columns, data
+    return columns, data
 
 def get_conditions(filters):
-	"Return conditions for query."
+    "Return conditions for query."
 
-	conditions = ""
+    conditions = ""
 
-	if filters.get("customer"):
-		conditions += f" and customer = '{filters['customer']}'"
-	if filters.get("customer_code"):
-		conditions += f" and customer_code like '%{filters['customer_code']}%'"
-	if filters.get("currency"):
-		conditions += f" and currency = '{filters['currency']}'"
-	conditions += f" and (posting_date >= '{filters['start_date']}' and posting_date <= '{filters['end_date']}')"
+    if filters.get("customer"):
+        conditions += f" and customer = '{filters['customer']}'"
+    if filters.get("customer_name"):
+        conditions += f" and customer_name like '%{filters['customer_name']}%'"
+    if filters.get("currency"):
+        conditions += f" and currency = '{filters['currency']}'"
+    conditions += f" and (posting_date >= '{filters['start_date']}' and posting_date <= '{filters['end_date']}')"
 
-	return conditions
+    return conditions
