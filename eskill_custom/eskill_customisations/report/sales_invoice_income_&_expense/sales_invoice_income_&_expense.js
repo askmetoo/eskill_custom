ead7f7c645a777c649e2139f7853d6b44ae4acac
Archais <aipt321@gmail.com>
2022-01-26 06:04:06 +0000

    Remade report to improve accuracy and bring coding style in line with the rest of the repo.

--- eskill_custom/eskill_customisations/report/sales_invoice_income_&_expense/sales_invoice_income_&_expense.js
+++ eskill_custom/eskill_customisations/report/sales_invoice_income_&_expense/sales_invoice_income_&_expense.js
@@ -1,35 +1,66 @@
 frappe.query_reports["Sales Invoice Income & Expense"] = {
-    "filters": [
+    filters: [
         {
-            "fieldname":"customer",
-            "label": __("Customer"),
-            "fieldtype": "Link",
-            "options": "Customer"
+            fieldname: "customer",
+            label:  __("Customer"),
+            fieldtype:  "Link",
+            options:  "Customer"
         },
         {
-            "fieldname":"customer_name",
-            "label": __("Customer Name"),
-            "fieldtype": "Data",
+            fieldname: "customer_name",
+            label:  __("Customer Name"),
+            fieldtype:  "Data",
         },
         {
-            "fieldname":"currency",
-            "label": __("Currency"),
-            "fieldtype": "Link",
-            "options": "Currency"
+            fieldname: "currency",
+            label:  __("Currency"),
+            fieldtype:  "Link",
+            options:  "Currency"
         },
         {
-            "fieldname":"start_date",
-            "label": __("Start Date"),
-            "fieldtype": "Date",
-            "reqd": 1,
-            "default": frappe.datetime.month_start()
+            fieldname: "sales_person",
+            label:  __("Sales Person"),
+            fieldtype:  "Link",
+            options:  "Sales Person"
         },
         {
-            "fieldname":"end_date",
-            "label": __("End Date"),
-            "fieldtype": "Date",
-            "reqd": 1,
-            "default": frappe.datetime.month_end()
+            fieldname: "start_date",
+            label:  __("Start Date"),
+            fieldtype:  "Date",
+            reqd:  1,
+            default:  frappe.datetime.month_start()
         },
-    ]
+        {
+            fieldname: "end_date",
+            label:  __("End Date"),
+            fieldtype:  "Date",
+            reqd:  1,
+            default:  frappe.datetime.month_end()
+        },
+        {
+            fieldname: "minimum_gp",
+            label: __("Minimum GP %"),
+            fieldtype: "Percent",
+            default: 15,
+            reqd: 1
+        },
+        {
+            fieldname: "secondary_currency",
+            fieldtype: "Link",
+            options: "Currency",
+            default: "ZWL",
+            hidden: 1
+        }
+    ],
+    formatter: function(value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
+        if (data.gross_profit < data.minimum_gp) {
+            value = "<span style='color:red!important;'>" + value + "</span>";
+        }
+        if (!data.invoice) {
+            value = ""
+        }
+        return value;
+    },
+    name_field: "invoice"
 }
--- eskill_custom/eskill_customisations/report/sales_invoice_income_&_expense/sales_invoice_income_&_expense.py
+++ eskill_custom/eskill_customisations/report/sales_invoice_income_&_expense/sales_invoice_income_&_expense.py
@@ -2,155 +2,278 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from datetime import datetime
+
 import frappe
 from frappe import _
 
 
-def execute(filters=None):
-    if filters:
-        conditions = get_conditions(filters)
-    else:
-        conditions= ''
+def execute(filters: dict = None):
+    "Generate report."
 
-    columns = [{
-            "fieldname": "invoice",
-            "label": _("Invoice"),
-            "fieldtype": "Link",
-            "options": "Sales Invoice",
-            "width": 100
-        },
-        {
-            "fieldname": "customer",
-            "label": _("Customer"),
-            "fieldtype": "Link",
-            "options": "Customer",
-            "width": 400
-        },
-        {
-            "fieldname": "customer_name",
-            "label": _("Customer Code"),
-            "fieldtype": "Text",
-            "width": 100
-        },
-        {
-            "fieldname": "posting_date",
-            "label": _("Posting Date"),
-            "fieldtype": "Date",
-            "width": 120
-        },
-        {
-            "fieldname": "currency",
-            "label": _("Invoice Currency"),
-            "fieldtype": "Text",
-            "width": 50
-        },
-        {
-            "fieldname": "cos_usd",
-            "label": _("Cost of Sales (USD)"),
-            "fieldtype": "Currency",
-            "width": 120
-        },
-        {
-            "fieldname": "net_total_usd",
-            "label": _("Income Excl. VAT (USD)"),
-            "fieldtype": "Currency",
-            "width": 120
-        },
-        {
-            "fieldname": "total_usd",
-            "label": _("Income Incl. VAT (USD)"),
-            "fieldtype": "Currency",
-            "width": 120
-        },
-        {
-            "fieldname": "rate",
-            "label": _("Rate (Auction/Exchange)"),
-            "fieldtype": "Text",
-            "width": 60
-        },
-        {
-            "fieldname": "cos_zwd",
-            "label": _("Cost of Sales (ZWD)"),
-            "fieldtype": "Currency",
-            "width": 120
-        },
-        {
-            "fieldname": "net_total_zwd",
-            "label": _("Income Excl. VAT (ZWD)"),
-            "fieldtype": "Currency",
-            "options": "",
-            "width": 120
-        },
-        {
-            "fieldname": "total_zwd",
-            "label": _("Income Incl. VAT (ZWD)"),
-            "fieldtype": "Currency",
-            "width": 120
-        }
-    ]
-    data = frappe.db.sql(f"""select
-    `tabSales Invoice`.name as invoice,
-    `tabSales Invoice`.customer as customer,
-    tabCustomer.customer_name as customer_name,
-    `tabSales Invoice`.posting_date as posting_date,
-    `tabSales Invoice`.currency as currency,
-    (case when `tabSales Invoice`.is_return then GL.credit * -1 else GL.debit end) as cos_usd,
-    `tabSales Invoice`.base_net_total as net_total_usd,
-    `tabSales Invoice`.base_total as total_usd,
-    convert(rate.rate, char) as rate,
-    (case when `tabSales Invoice`.is_return then (GL.credit * rate.rate) * -1 else (GL.debit * rate.rate) end) as cos_zwd,
-    (`tabSales Invoice`.base_net_total * rate.rate) as net_total_zwd,
-    (`tabSales Invoice`.base_total * rate.rate) as total_zwd
-from
-    `tabSales Invoice`
-left join
-    tabCustomer on tabCustomer.name = `tabSales Invoice`.customer 
-join
-    (select
-        parent invoice,
-        delivery_note
-    from
-        `tabSales Invoice Item`
-    where
-        item_code is not null
-    group by
-        invoice) as link on `tabSales Invoice`.name = link.invoice
-left join
-    (select
-        voucher_no,
-        sum(debit) debit,
-        sum(credit) credit
-    from
-        `tabGL Entry`
-    where
-        account like '%Cost of Sales%' and voucher_type = 'Delivery Note'
-    group by
-        voucher_no) as GL on GL.voucher_no = link.delivery_note
-join
-    (select
-        name invoice,
-        (case when currency = 'USD' then auction_bid_rate else 1 / conversion_rate end) rate
-    from
-        `tabSales Invoice`) as rate on rate.invoice = `tabSales Invoice`.name 
-where
-    `tabSales Invoice`.docstatus = 1 {conditions}
-order by
-    `tabSales Invoice`.name desc
-""", as_dict=1)
+    columns = get_columns(filters)
+
+    data = get_data(filters, columns)
+
+    for i, row in enumerate(data):
+        data[i]['report_currency'] = filters['secondary_currency']
 
     return columns, data
 
-def get_conditions(filters):
-    "Return conditions for query."
 
-    conditions = ""
+def get_columns(filters: dict) -> 'list[dict]':
+    "Returns a list of columns for the report."
 
-    if filters.get("customer"):
-        conditions += f" and customer = '{filters['customer']}'"
-    if filters.get("customer_name"):
-        conditions += f" and customer_name like '%{filters['customer_name']}%'"
-    if filters.get("currency"):
-        conditions += f" and currency = '{filters['currency']}'"
-    conditions += f" and (posting_date >= '{filters['start_date']}' and posting_date <= '{filters['end_date']}')"
+    columns = [
+        {
+            'fieldname': "invoice",
+            'label': _("Invoice"),
+            'fieldtype': "Link",
+            'options': "Sales Invoice",
+            'width': 110
+        },
+        {
+            'fieldname': "customer",
+            'label': _("Customer"),
+            'fieldtype': "Link",
+            'options': "Customer",
+            'width': 110
+        },
+        {
+            'fieldname': "customer_name",
+            'label': _("Customer Code"),
+            'fieldtype': "Text",
+            'width': 400
+        },
+        {
+            'fieldname': "posting_date",
+            'label': _("Posting Date"),
+            'fieldtype': "Date",
+            'width': 120
+        },
+        {
+            'fieldname': "currency",
+            'label': _("Invoice Currency"),
+            'fieldtype': "Text",
+            'width': 50
+        },
+        {
+            'fieldname': "sales_person",
+            'label': _("Sales Person"),
+            'fieldtype': "Link",
+            'options': "Sales Person"
+        },
+        {
+            'fieldname': "cos",
+            'label': _("Cost of Sales (USD)"),
+            'fieldtype': "Currency",
+            'options': "Company:company:default_currency",
+            'width': 120
+        },
+        {
+            'fieldname': "net_total",
+            'label': _("Income Excl. VAT (USD)"),
+            'fieldtype': "Currency",
+            'options': "Company:company:default_currency",
+            'width': 120
+        },
+        {
+            'fieldname': "total",
+            'label': _("Income Incl. VAT (USD)"),
+            'fieldtype': "Currency",
+            'options': "Company:company:default_currency",
+            'width': 120
+        },
+        {
+            'fieldname': "rate",
+            'label': _("Rate (Auction/Exchange)"),
+            'fieldtype': "Float",
+            'precision': 4
+        },
+        {
+            'fieldname': "cos_secondary",
+            'label': _(f"Cost of Sales ({filters['secondary_currency']})"),
+            'fieldtype': "Currency",
+            'options': "report_currency",
+            'width': 120
+        },
+        {
+            'fieldname': "net_total_secondary",
+            'label': _(f"Income Excl. VAT ({filters['secondary_currency']})"),
+            'fieldtype': "Currency",
+            'options': "report_currency",
+            'width': 120
+        },
+        {
+            'fieldname': "total_secondary",
+            'label': _(f"Income Incl. VAT ({filters['secondary_currency']})"),
+            'fieldtype': "Currency",
+            'options': "report_currency",
+            'width': 120
+        },
+        {
+            'fieldname': "gross_profit",
+            'label': _("Gross Profit %"),
+            'fieldtype': "Percent"
+        }
+    ]
 
-    return conditions
+    return columns
+
+
+def get_data(filters: dict, columns: 'list[dict]') -> 'list[dict]':
+    "Generates report data."
+
+    data = initialise_data(filters, columns)
+
+    if "customer" in filters:
+        data = [
+            row
+            for i, row in enumerate(data)
+            if row['customer'] == filters['customer']
+        ]
+
+    if "sales_person" in filters:
+        data = [
+            row
+            for i, row in enumerate(data)
+            if row['sales_person'] == filters['sales_person']
+        ]
+
+    if "currency" in filters:
+        data = [
+            row
+            for i, row in enumerate(data)
+            if row['currency'] == filters['currency']
+        ]
+
+    if "start_date" in filters:
+        start_date = datetime.strptime(filters['start_date'], "%Y-%m-%d").date()
+        data = [
+            row
+            for i, row in enumerate(data)
+            if row['posting_date'] >= start_date
+        ]
+    if "end_date" in filters:
+        end_date = datetime.strptime(filters['end_date'], "%Y-%m-%d").date()
+        data = [
+            row
+            for i, row in enumerate(data)
+            if row['posting_date'] <= end_date
+        ]
+
+    invoices = tuple({row['invoice'] for row in data})
+
+    if len(invoices) > 0:
+        deliveries = get_deliveries(invoices)
+        for delivery in deliveries:
+            index = next(i for i, row in enumerate(data) if row['invoice'] == delivery['invoice'])
+            data[index]['cos'] = delivery['cos']
+            data[index]['cos_secondary'] = delivery['cos'] * data[index]['rate']
+
+        for i, row in enumerate(data):
+            data[i]['gross_profit'] = (row['total'] - row['cos']) / row['total'] * 100
+            data[i]['minimum_gp'] = filters['minimum_gp']
+
+        total_row = {'invoice': "Total", 'total': 1}
+        for column in columns:
+            if column['fieldtype'] == "Currency":
+                total_row[column['fieldname']] = 0
+                for i, row in enumerate(data):
+                    total_row[column['fieldname']] += row[column['fieldname']]
+        total_row['gross_profit'] = (
+            (total_row['total'] - total_row['cos'])
+            / total_row['total']
+            * 100
+        )
+        data.extend(({}, total_row))
+
+    return data
+
+
+def initialise_data(filters: dict, columns: 'list[dict]'):
+    "Initialise report data."
+
+    if "customer_name" in filters:
+        customer_filter = f"and C.customer_name like '%{filters['customer_name']}%'"
+    else:
+        customer_filter = ""
+
+    data = frappe.db.sql(f"""
+        select
+            SI.name invoice,
+            SI.customer,
+            C.customer_name,
+            SI.posting_date,
+            SI.currency,
+            tab1.sales_person,
+            (case when
+                SI.currency = 'ZWL'
+            then
+                1 / SI.conversion_rate
+            else
+                SI.auction_bid_rate
+            end) rate,
+            SI.base_net_total net_total,
+            SI.base_grand_total total,
+            (case when
+                SI.currency = 'ZWL'
+            then
+                SI.net_total
+            else
+                SI.base_net_total * SI.auction_bid_rate
+            end) net_total_secondary,
+            (case when
+                SI.currency = 'ZWL'
+            then
+                SI.grand_total
+            else
+                SI.base_grand_total * SI.auction_bid_rate
+            end) total_secondary
+        from
+            `tabSales Invoice` SI
+        join
+            tabCustomer C on SI.customer = C.name
+        left join
+            (select
+                parent,
+                sales_person,
+                max(allocated_percentage)
+            from
+                `tabSales Team`
+            group by
+                parent) tab1 on SI.name = tab1.parent
+        where
+            SI.docstatus = 1
+            and is_opening = 'No'
+            {customer_filter}
+        order by
+            SI.posting_date;""", as_dict=1)
+
+    for i, row in enumerate(data):
+        for column in columns:
+            if column['fieldname'] not in row:
+                data[i][column['fieldname']] = 0
+
+    return data
+
+
+def get_deliveries(invoices: tuple):
+    "Get cost of sales information from delivery notes."
+
+    data = frappe.db.sql(f"""
+        select
+            SII.parent invoice,
+            sum(DNI.incoming_rate * SII.qty) cos
+        from
+            `tabSales Invoice Item` SII
+        join
+            `tabDelivery Note Item` DNI on SII.dn_detail = DNI.name
+        where
+            SII.parent in {invoices}
+        group by
+            SII.parent
+        having
+            cos <> 0;""", as_dict=1)
+
+    return data
