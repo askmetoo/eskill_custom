58a0b1d847d3da3acfb304012b50fce1ce9b4e12
Archais <aipt321@gmail.com>
2022-02-04 11:49:24 +0200

    Added reason_on_hold field to Service Order and new Service Order on Hold Type DocType so that users can provide a reason for putting the job on hold. reason_on_hold will display as part of the status in the listview.

--- eskill_custom/eskill_customisations/doctype/service_order/service_order_list.js
+++ eskill_custom/eskill_customisations/doctype/service_order/service_order_list.js
@@ -2,29 +2,38 @@
 // For license information, please see license.txt
 
 frappe.listview_settings['Service Order'] = {
-    add_fields: ["billing_status", "job_status", "job_type"],
+    add_fields: ["billing_status", "customer_name", "job_status", "job_type", "reason_on_hold"],
+    colwidths: {
+        name: 1
+    },
     get_indicator(doc) {
-        var doc_status = null
+        var doc_status = doc.job_status + ": ";
+        var colour = "green";
         if (doc.job_status == "Open") {
-            return [__(doc.job_status + ": " + doc.job_type), "green",]
-        } else if (doc.job_type == "Billable") {
-            doc_status = __(doc.job_status + ": " + doc.billing_status)
-            if (doc.job_status == "On Hold") {
-                return [doc_status, "orange"]
-            }
-            const status_color = {
-                "Pending Billing": "red",
-                "Delivered": "blue",
-                "Invoiced": "dark grey",
-            };
-            return [doc_status, status_color[doc.billing_status]]
+            doc_status = __(doc_status + doc.job_type);
+        } else if (doc.job_status == "On Hold") {
+            doc_status = __(doc_status + doc.reason_on_hold);
+            colour = "orange";
         } else {
-            return [__(doc.job_status + ": " + doc.job_type), "dark grey"]
+            doc_status = __(doc_status + doc.billing_status)
+            if (doc.job_type == "Billable") {
+                colour = {
+                    "Pending Billing": "red",
+                    "Delivered": "blue",
+                    "Invoiced": "dark grey",
+                }[doc.billing_status];
+            } else {
+                colour = {
+                    "Pending Billing": "red",
+                    "Delivered": "dark grey",
+                }[doc.billing_status];
+            }
         }
+        return [doc_status, colour];
     },
     hide_name_column: true,
     refresh(list) {
-        set_breadcrumbs(list)
+        set_breadcrumbs(list);
     }
 };
 
