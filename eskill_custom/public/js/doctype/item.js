6a47a6262d95ed79d15a0d0ac091f7b64cfca2fe
Archais <aipt321@gmail.com>
2022-03-16 08:39:19 +0200

    Removed KBA creation buttons from prototype knowledge base.

--- eskill_custom/public/js/doctype/item.js
+++ eskill_custom/public/js/doctype/item.js
@@ -1,8 +1,4 @@
 frappe.ui.form.on('Item', {
-    refresh(frm) {
-        kba_buttons(frm);
-    },
-
     onload(frm) {
         account_filter(frm);
     }
@@ -31,24 +27,4 @@ function account_filter(frm) {
             }
         }
     };
-}
-
-
-function kba_buttons(frm) {
-    frm.add_custom_button(__("KB Articles"), function () {
-        frm.save();
-        frappe.route_options = {
-            "type": "Product",
-            "product": cur_frm.doc.item_code
-        };
-        frappe.set_route("List", "KBA", "List");
-    });
-    frm.add_custom_button(__("New KBA"), function () {
-        frm.save();
-        frappe.route_options = {
-            "type": "Product",
-            "product": cur_frm.doc.item_code
-        };
-        frappe.set_route("Form", "KBA", "New KBA");
-    });
 }
