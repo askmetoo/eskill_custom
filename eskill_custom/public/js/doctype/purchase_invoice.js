903b15c5f45102da396abf8817e8f00e0941950a
Archais <aipt321@gmail.com>
2022-03-28 13:58:36 +0200

    Implemented usd_to_currency field on purchase documents.

--- eskill_custom/public/js/doctype/purchase_invoice.js
+++ eskill_custom/public/js/doctype/purchase_invoice.js
@@ -5,5 +5,13 @@ frappe.require([
 frappe.ui.form.on('Purchase Invoice', {
     before_submit(frm) {
         get_bid_rate(frm, frm.doc.posting_date);
+    },
+
+    conversion_rate(frm) {
+        convert_selected_to_base(frm);
+    },
+
+    usd_to_currency(frm) {
+        convert_base_to_selected(frm);
     }
 });
--- eskill_custom/public/js/doctype/purchase_order.js
+++ eskill_custom/public/js/doctype/purchase_order.js
@@ -2,4 +2,12 @@ frappe.require([
     '/assets/eskill_custom/js/common.js'
 ]);
 
-frappe.ui.form.on('Purchase Receipt', {});
+frappe.ui.form.on('Purchase Order', {
+    conversion_rate(frm) {
+        convert_selected_to_base(frm);
+    },
+
+    usd_to_currency(frm) {
+        convert_base_to_selected(frm);
+    }
+});
