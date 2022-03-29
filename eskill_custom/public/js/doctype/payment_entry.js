13037d3c5ca5cae1481682a26e1da9b818d536fd
Archais <aipt321@gmail.com>
2022-03-29 10:46:19 +0200

    Implemented fields to allow setting the exchange rate from base currency to the selected currency for both the source and target.

--- eskill_custom/public/js/doctype/payment_entry.js
+++ eskill_custom/public/js/doctype/payment_entry.js
@@ -12,10 +12,60 @@ frappe.ui.form.on('Payment Entry', {
         get_bid_rate(frm, frm.doc.posting_date);
     },
     
-    payment_type: function(frm) {
+    payment_type(frm) {
         name_series(frm);
-    }
+    },
+
+    source_exchange_rate(frm) {
+        convert_selected_to_base(frm, true);
+    },
+
+    source_usd_to_currency(frm) {
+        convert_base_to_selected(frm, true);
+    },
+
+    target_exchange_rate(frm) {
+        convert_selected_to_base(frm, false);
+    },
+
+    target_usd_to_currency(frm) {
+        convert_base_to_selected(frm, false);
+    },
 });
+
+function convert_base_to_selected(frm, source) {
+    if (source) {
+        if (frm.doc.source_usd_to_currency) {
+            frm.set_value('source_exchange_rate', roundNumber(1 / frm.doc.source_usd_to_currency, 9));
+        } else {
+            frm.set_value('source_exchange_rate', null);
+        }
+    } else {
+        if (frm.doc.target_usd_to_currency) {
+            frm.set_value('target_exchange_rate', roundNumber(1 / frm.doc.target_usd_to_currency, 9));
+        } else {
+            frm.set_value('target_exchange_rate', null);
+        }
+    }
+}
+
+function convert_selected_to_base(frm, source) {
+    if (source) {
+        if (frm.doc.source_exchange_rate) {
+            frm.doc.source_usd_to_currency = roundNumber(1 / frm.doc.source_exchange_rate, 4);
+            frm.refresh_field('source_usd_to_currency');
+        } else {
+            frm.set_value('source_usd_to_currency', null);
+        }
+    } else {
+        if (frm.doc.target_exchange_rate) {
+            frm.doc.target_usd_to_currency = roundNumber(1 / frm.doc.target_exchange_rate, 4);
+            frm.refresh_field('target_usd_to_currency');
+        } else {
+            frm.set_value('target_usd_to_currency', null);
+        }
+    }
+}
 
 function name_series(frm) {
     if (frm.doc.payment_type == "Receive") {
