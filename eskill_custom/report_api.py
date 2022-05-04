fddfc65529cc1804a9b076353e2ffca5a27ae2ba
Archais <aipt321@gmail.com>
2022-05-04 16:16:30 +0200

    Implemented function that returns a set listing all descendants of a given node of a tree structured DocType.

--- eskill_custom/report_api.py
+++ eskill_custom/report_api.py
@@ -1,1 +1,31 @@
 "Generic methods that can be used in various Reports."
+
+
+import frappe
+
+
+def get_descendants(doctype: str, name: str, parent_field: str = "parent") -> set:
+    "Returns a set of all descendants of a given Document."
+
+    descendants = set()
+    meta = frappe.get_doc("DocType", doctype)
+    if not meta.is_tree:
+        return descendants
+
+    child_docs = frappe.get_all(
+        doctype=doctype,
+        filters={
+            parent_field: name
+        },
+        fields=[
+            "name",
+            "is_group"
+        ]
+    )
+    for child in child_docs:
+        descendants.add(child.name)
+        if child.is_group:
+            for sub_child in get_descendants(doctype, child.name):
+                descendants.add(sub_child)
+
+    return descendants
