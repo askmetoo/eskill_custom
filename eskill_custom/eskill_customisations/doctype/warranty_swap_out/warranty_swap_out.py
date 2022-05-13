316eeed6cc0d2513471a827ff813738b39aa481f
Archais <aipt321@gmail.com>
2022-05-13 14:15:13 +0200

    The method update_service_order() is now run entirely on the server-side. It is run during the on_submit() and after_cancel() events.

--- eskill_custom/eskill_customisations/doctype/warranty_swap_out/warranty_swap_out.py
+++ eskill_custom/eskill_customisations/doctype/warranty_swap_out/warranty_swap_out.py
@@ -9,10 +9,22 @@ from frappe.model.document import Document
 
 
 class WarrantySwapOut(Document):
-    @frappe.whitelist()
+    "Collection of methods for Warranty Swap Out DocType."
+
+    def on_submit(self):
+        "Method run after submitting the document."
+        self.update_service_order()
+
+
+    def after_cancel(self):
+        "Method run after cancelling the document."
+        self.update_service_order()
+
+
     def update_service_order(self):
+        "Update the linked Service Order."
         service_device = frappe.get_doc("Service Device", self.service_device)
         if self.docstatus != 2:
-            service_device.db_set("swap_out", self.name)
+            service_device.db_set("swap_out", self.name, notify=True)
         else:
-            service_device.db_set("swap_out", None)
+            service_device.db_set("swap_out", None, notify=True)
