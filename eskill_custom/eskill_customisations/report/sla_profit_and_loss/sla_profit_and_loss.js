65307f1837bf9a8eaa105de06bcbab3467b5debf
Archais <aipt321@gmail.com>
2022-07-11 06:15:30 +0200

    Added graphs and implemented more advanced filters.

--- eskill_custom/eskill_customisations/report/sla_profit_and_loss/sla_profit_and_loss.js
+++ eskill_custom/eskill_customisations/report/sla_profit_and_loss/sla_profit_and_loss.js
@@ -2,6 +2,14 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
+frappe.form.link_formatters['Customer'] = (value, doc) => {
+    if (value && doc.customer_name && (doc.customer_name != value) && (value == doc.customer)) {
+        return value + ": " + doc.customer_name;
+    } else {
+        return value;
+    }
+}
+
 frappe.query_reports["SLA Profit and Loss"] = {
     'filters': [
         {
@@ -9,6 +17,18 @@ frappe.query_reports["SLA Profit and Loss"] = {
             label: __("SLA"),
             fieldtype: "Link",
             options: "Device SLA",
+        },
+        {
+            fieldname: "contract_tier",
+            label: __("Contract Tier"),
+            fieldtype: "Link",
+            options: "SLA Level"
+        },
+        {
+            fieldname: "year",
+            label: __("Year"),
+            fieldtype: "Link",
+            options: "Fiscal Year"
         },
         {
             fieldname: "customer",
@@ -24,14 +44,26 @@ frappe.query_reports["SLA Profit and Loss"] = {
             }
         },
         {
-            fieldname: "contract_tier",
-            label: __("Contract Tier"),
-            fieldtype: "Link",
-            options: "SLA Level"
-        },
-        {
             fieldname: "customer_name",
             label: __("Customer Name"),
+            fieldtype: "Data"
+        },
+        {
+            fieldname: "model",
+            label: __("Model"),
+            fieldtype: "Link",
+            options: "Item",
+            get_query() {
+                return {
+                    filters: [
+                        ["Item", "has_serial_no", "=", 1]
+                    ]
+                }
+            }
+        },
+        {
+            fieldname: "model_name",
+            label: __("Model Name"),
             fieldtype: "Data"
         },
         {
@@ -39,5 +71,18 @@ frappe.query_reports["SLA Profit and Loss"] = {
             label: __("Monthly Breakdown"),
             fieldtype: "Check"
         },
-    ]
+    ],
+    formatter(value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
+        if (data) {
+            if (
+                (column.fieldname == "days_remaining" && data.days_remaining <= 30)
+                || (column.fieldtype == "Currency" && data[column.fieldname] < 0)
+            ) {
+                value = "<span style='color:red!important;'>" + value + "</span>";
+            }
+        }
+        return value;
+    },
+    name_field: "sla"
 };
