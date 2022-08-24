679fc34ef2788202e9b057d7160ba6e2de83d6bd
Archais <aipt321@gmail.com>
2022-08-24 16:20:55 +0200

    Added update_window() method to PriceLookup class.

--- eskill_custom/public/js/utils/price_lookup.js
+++ eskill_custom/public/js/utils/price_lookup.js
@@ -3,12 +3,12 @@
 
 frappe.provide("eskill_custom.ui");
 
-// params are pointless right now, need to figure out why they're not working
+// define size and postion of the window
 const LOOKUP_WINDOW_PARAMS = `
     width = ${screen.width},
-    height = 300,
+    height = 250,
     left = 0,
-    top = ${screen.height - 300}
+    top = ${screen.height - 250}
 `;
 
 class PriceLookup {
@@ -29,6 +29,31 @@ class PriceLookup {
             return true;
         }
         return false;
+    }
+
+    update_window(item_code) {
+        // this method update the item_field in the price-lookup page
+        const is_closed = this.closed();
+
+        this.open();
+        
+        if (is_closed) {
+            // if the window was not already open, add an event onload to set the item code
+            this.window.addEventListener("load", () => {
+                try {
+                    // for some reason some browsers have cur_page loaded up in its entirety by the time the load event is triggered
+                    this.window.cur_page.page.price_lookup.page.item_field.set_value(item_code);
+                } catch (error) {
+                    // whilst others do not, if the browser does not load cur_page in time, set a timeout of 1.5s and try again
+                    setTimeout(() => {
+                        this.window.cur_page.page.price_lookup.page.item_field.set_value(item_code);
+                    }, 1500);
+                }
+            }, true);
+        } else {
+            // otherwise immediately set the item code
+            this.window.cur_page.page.price_lookup.page.item_field.set_value(item_code);
+        }
     }
 }
 
