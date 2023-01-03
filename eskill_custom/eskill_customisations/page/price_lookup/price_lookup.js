2f9a40f63701d24aa48e103735fa1bd46e4b9943
Archais <aipt321@gmail.com>
2023-01-03 09:54:05 +0200

    Updated price_lookup.js formatting using Prettier.

--- eskill_custom/eskill_customisations/page/price_lookup/price_lookup.js
+++ eskill_custom/eskill_customisations/page/price_lookup/price_lookup.js
@@ -1,20 +1,20 @@
 // specification of borders used in the table for uniformity
-const BORDER_SPEC = "solid 3px var(--border-color)"
+const BORDER_SPEC = "solid 3px var(--border-color)";
 
 // tax rate used for calculating the minimum selling price
 const TAX_RATE = 1.15;
 
-frappe.pages['price-lookup'].on_page_load = function(wrapper) {
-	let page = frappe.ui.make_app_page({
-		parent: wrapper,
-		show_sidebar: 0,
-		single_column: true
-	});
+frappe.pages["price-lookup"].on_page_load = function (wrapper) {
+  let page = frappe.ui.make_app_page({
+    parent: wrapper,
+    show_sidebar: 0,
+    single_column: true,
+  });
 
-	const styles = document.createElement("style");
-	styles.setAttribute("type", "text/css");
+  const styles = document.createElement("style");
+  styles.setAttribute("type", "text/css");
 
-	styles.innerHTML = `
+  styles.innerHTML = `
 		.column-head {
 			font-weight: bold;
 			border-bottom: ${BORDER_SPEC};
@@ -39,139 +39,166 @@ frappe.pages['price-lookup'].on_page_load = function(wrapper) {
 		.no-info {
 			color: var(--gray);
 		}
-	`
-	
-	document.head.insertAdjacentElement("beforeend", styles);
-	// document.getElementsByClassName("page-head")[0].remove(); // this statement needs to commented out if you want to display any buttons
-	document.getElementsByClassName("page-head-content")[0].setAttribute("style", "height: 35px;");
-	
-	$("<div class='price-lookup'></div>").appendTo(page.main);
-	wrapper.price_lookup = new PriceLookupPage(wrapper);
-}
+	`;
+
+  document.head.insertAdjacentElement("beforeend", styles);
+  // document.getElementsByClassName("page-head")[0].remove(); // this statement needs to commented out if you want to display any buttons
+  document
+    .getElementsByClassName("page-head-content")[0]
+    .setAttribute("style", "height: 35px;");
+
+  $("<div class='price-lookup'></div>").appendTo(page.main);
+  wrapper.price_lookup = new PriceLookupPage(wrapper);
+};
 
 class PriceLookupPage {
-	constructor(wrapper) {
-		this.wrapper = wrapper;
-		this.page = wrapper.page;
-		this.body = $(this.wrapper).find(".price-lookup");
-		this.min_gp_rate = 0.18;
+  constructor(wrapper) {
+    this.wrapper = wrapper;
+    this.page = wrapper.page;
+    this.body = $(this.wrapper).find(".price-lookup");
+    this.min_gp_rate = 0.18;
 
-		if(!window.hasOwnProperty("current_item")) window.current_item = undefined;
+    if (!window.hasOwnProperty("current_item")) window.current_item = undefined;
 
-		this.make();
-		this.get_data();
+    this.make();
+    this.get_data();
 
-		// refresh table continuously as long as the item is unchanged
-		setInterval(() => {
-			if (current_item && this.page.match_price_check.value) {
-				if (this.page.item_field.value == current_item.item_obj[current_item.item_field]) {
-					this.page.base_selling_price.set_value(current_item.item_obj[current_item.base_rate_field]);
-				}
-			}
-		}, 5000);
+    // refresh table continuously as long as the item is unchanged
+    setInterval(() => {
+      if (current_item && this.page.match_price_check.value) {
+        if (
+          this.page.item_field.value ==
+          current_item.item_obj[current_item.item_field]
+        ) {
+          this.page.base_selling_price.set_value(
+            current_item.item_obj[current_item.base_rate_field]
+          );
+        }
+      }
+    }, 5000);
 
-		// add button to resync lookup dialog with the last selected item
-		this.page.add_inner_button(__("Refresh"), () => {
-			if (!this.page.match_price_check.value) {
-				if (!current_item) {
-					frappe.msgprint('This lookup window is not linked to a specific item.<br>Please use the "Check Price" button to link to an item.');
-				} else {
-					this.page.match_price_check.set_value(1);
-					this.page.item_field.set_value(current_item.item_obj[current_item.item_field]);
-					this.page.base_selling_price.set_value(current_item.item_obj[current_item.base_rate_field]);
-				}
-			}
-		});
-	}
+    // add button to resync lookup dialog with the last selected item
+    this.page.add_inner_button(__("Refresh"), () => {
+      if (!this.page.match_price_check.value) {
+        if (!current_item) {
+          frappe.msgprint(
+            'This lookup window is not linked to a specific item.<br>Please use the "Check Price" button to link to an item.'
+          );
+        } else {
+          this.page.match_price_check.set_value(1);
+          this.page.item_field.set_value(
+            current_item.item_obj[current_item.item_field]
+          );
+          this.page.base_selling_price.set_value(
+            current_item.item_obj[current_item.base_rate_field]
+          );
+        }
+      }
+    });
+  }
 
-	make() {
-		const parent = this;
-		this.page.item_field = this.page.add_field({
-			fieldname: "item_code",
-			label: __("Item"),
-			fieldtype: "Link",
-			options: "Item",
-			change() {
-				if (current_item) {
-					if (this.value != current_item.item_obj[current_item.item_field]) {
-						parent.page.match_price_check.set_value(0);
-					}
-				}
+  make() {
+    const parent = this;
+    this.page.item_field = this.page.add_field({
+      fieldname: "item_code",
+      label: __("Item"),
+      fieldtype: "Link",
+      options: "Item",
+      change() {
+        if (current_item) {
+          if (this.value != current_item.item_obj[current_item.item_field]) {
+            parent.page.match_price_check.set_value(0);
+          }
+        }
 
-				frappe.call({
-					method: "eskill_custom.eskill_customisations.page.price_lookup.price_lookup.get_gp_rate",
-					args: {
-						item_code: this.value
-					},
-					callback(response) {
-						parent.min_gp_rate = response.message;
-						parent.get_data();
-					}
-				});
-			}
-		});
+        frappe.call({
+          method:
+            "eskill_custom.eskill_customisations.page.price_lookup.price_lookup.get_gp_rate",
+          args: {
+            item_code: this.value,
+          },
+          callback(response) {
+            parent.min_gp_rate = response.message;
+            parent.get_data();
+          },
+        });
+      },
+    });
 
-		this.page.base_selling_price = this.page.add_field({
-			fieldname: "base_selling_price",
-			label: __("Base Selling Price"),
-			fieldtype: "Currency",
-			change() {
-				if (current_item) {
-					if (this.value != current_item.item_obj[current_item.base_rate_field]) {
-						parent.page.match_price_check.set_value(0);
-					}
-				}
+    this.page.base_selling_price = this.page.add_field({
+      fieldname: "base_selling_price",
+      label: __("Base Selling Price"),
+      fieldtype: "Currency",
+      change() {
+        if (current_item) {
+          if (
+            this.value != current_item.item_obj[current_item.base_rate_field]
+          ) {
+            parent.page.match_price_check.set_value(0);
+          }
+        }
 
-				frappe.call({
-					method: "eskill_custom.eskill_customisations.page.price_lookup.price_lookup.get_gp_rate",
-					args: {
-						item_code: parent.page.item_field.value
-					},
-					callback(response) {
-						parent.min_gp_rate = response.message;
-						parent.get_data();
-					}
-				});
-			}
-		});
+        frappe.call({
+          method:
+            "eskill_custom.eskill_customisations.page.price_lookup.price_lookup.get_gp_rate",
+          args: {
+            item_code: parent.page.item_field.value,
+          },
+          callback(response) {
+            parent.min_gp_rate = response.message;
+            parent.get_data();
+          },
+        });
+      },
+    });
 
-		this.page.match_price_check = this.page.add_field({
-			fieldname: "match_price_check",
-			fieldtype: "Check",
-			default: 0,
-			hidden: 1
-		});
-	}
+    this.page.match_price_check = this.page.add_field({
+      fieldname: "match_price_check",
+      fieldtype: "Check",
+      default: 0,
+      hidden: 1,
+    });
+  }
 
-	get_data() {
-		frappe.db.get_list('Bin', {
-			fields: [
-				"warehouse",
-				"actual_qty",
-				"reserved_qty",
-				"ordered_qty",
-				"valuation_rate"
-			],
-			filters: {
-				item_code: this.page.item_field.value
-			}
-		}).then((bins) => {
-			let html_body = "";
-			const table_rows = [];
-			// const last_details = bins.length > 0 ? bins[0] : {};
+  get_data() {
+    frappe.db
+      .get_list("Bin", {
+        fields: [
+          "warehouse",
+          "actual_qty",
+          "reserved_qty",
+          "ordered_qty",
+          "valuation_rate",
+        ],
+        filters: {
+          item_code: this.page.item_field.value,
+        },
+      })
+      .then((bins) => {
+        let html_body = "";
+        const table_rows = [];
+        // const last_details = bins.length > 0 ? bins[0] : {};
 
-			for (let i = 0; i < bins.length; i++) {
-				const current_bin = bins[i];
+        for (let i = 0; i < bins.length; i++) {
+          const current_bin = bins[i];
 
-				if ((current_bin.actual_qty + current_bin.reserved_qty + current_bin.ordered_qty) == 0) {
-					continue;
-				}
+          if (
+            current_bin.actual_qty +
+              current_bin.reserved_qty +
+              current_bin.ordered_qty ==
+            0
+          ) {
+            continue;
+          }
 
-				const selling_price = Math.ceil(current_bin.valuation_rate / (1 - this.min_gp_rate) * TAX_RATE);
-				const net_price = this.page.base_selling_price.value / TAX_RATE;
-				const current_gp_rate = (net_price - current_bin.valuation_rate) / net_price;
+          const selling_price = Math.ceil(
+            (current_bin.valuation_rate / (1 - this.min_gp_rate)) * TAX_RATE
+          );
+          const net_price = this.page.base_selling_price.value / TAX_RATE;
+          const current_gp_rate =
+            (net_price - current_bin.valuation_rate) / net_price;
 
-				table_rows.push(`
+          table_rows.push(`
 					<tr>
 						<td class="column-content">
 							${current_bin.warehouse}
@@ -186,20 +213,22 @@ class PriceLookupPage {
 							${current_bin.ordered_qty}
 						</td>
 						<td class="column-content">
-							${frappe.format(current_bin.valuation_rate, {fieldtype: "Currency"})}
+							${frappe.format(current_bin.valuation_rate, { fieldtype: "Currency" })}
 						</td>
 						<td class="column-content">
-							${frappe.format(selling_price, {fieldtype: "Currency"})}
+							${frappe.format(selling_price, { fieldtype: "Currency" })}
 						</td>
-						<td class="column-content column-right" ${current_gp_rate < this.min_gp_rate ? 'style="color: red;"' : ''}>
-							${frappe.format(current_gp_rate * 100, {fieldtype: "Percent"})}
+						<td class="column-content column-right" ${
+              current_gp_rate < this.min_gp_rate ? 'style="color: red;"' : ""
+            }>
+							${frappe.format(current_gp_rate * 100, { fieldtype: "Percent" })}
 						</td>
 					</tr>
-				`)
-			}
+				`);
+        }
 
-			if (bins.length > 0) {
-				html_body += `
+        if (bins.length > 0) {
+          html_body += `
 					<div class="page-form">
 						<table style="width: 100%;">
 							<thead>
@@ -230,20 +259,20 @@ class PriceLookupPage {
 							<tbody>
 				`;
 
-				for (let i = 0; i < table_rows.length; i++) {
-					html_body += table_rows[i];
-				}
+          for (let i = 0; i < table_rows.length; i++) {
+            html_body += table_rows[i];
+          }
 
-				html_body += "</tbody></table></div>"
-			} else {
-				html_body += `
+          html_body += "</tbody></table></div>";
+        } else {
+          html_body += `
 					<div class="page-form no-info">
 						There is no information to show...
 					</div>
-				`
-			}
+				`;
+        }
 
-			this.body[0].innerHTML = html_body;
-		});
-	}
+        this.body[0].innerHTML = html_body;
+      });
+  }
 }
